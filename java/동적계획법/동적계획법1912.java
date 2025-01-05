6a8860ffa3c738c0370906c696143c5f413b43d8
parkhongseok <123qkrghdtjr@naver.com>
2025-01-05 18:18:50 +0900

    dp

--- java/동적계획법/동적계획법1912.java
+++ java/동적계획법/동적계획법1912.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int[] A = new int[N];
