8c269fa239f6a31f41a21fa8ca1dd5dd42d3eb19
parkhongseok <123qkrghdtjr@naver.com>
2025-01-08 19:58:10 +0900

    ha..

---
+++ java/부르트포스/리모컨1107.java
@@ -0,0 +1,14 @@
+import java.io.InputStreamReader;
+
+class 리모컨1107 {
+  
+
+  
+  
+  public static void main(String args[]){
+    InputStreamReader isr = new InputStreamReader(System.in);
+
+
+
+  }
+}
