46af76de962d5edd98f4828272fe4dda20b3da4e
parkhongseok <123qkrghdtjr@naver.com>
2025-07-27 16:14:55 +0900

    hash set

--- javaTest/src/basic/영어끝말잇기.java
+++ javaTest/src/basic/영어끝말잇기.java
@@ -1,12 +1,38 @@
 package basic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 영어끝말잇기 {
-      public int[] solution(int n, String[] words) {
-        int[] answer = {};
+  public static int[] solution(int n, String[] words) {
+    int[] answer = {0, 0};
+    Set<String> isOccupied = new HashSet<>();
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
-        System.out.println("Hello Java");
+    isOccupied.add(words[0]);
+    for (int i = 1; i < words.length; i++){
+      String before = words[i-1];
+      String now = words[i];
 
+      if ((before.charAt(before.length()-1) != now.charAt(0)) || isOccupied.contains(now)){
+        answer[0] = i%n + 1; 
+        answer[1] = i/n + 1;
         return answer;
+      }
+
+      isOccupied.add(now);
     }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int n = 2;
+    String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
+    int[] L = solution(n, words);
+
+    for (int x : L){
+      System.out.println(x);
+    }
+
+  }
 }
